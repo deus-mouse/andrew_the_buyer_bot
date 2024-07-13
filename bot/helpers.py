ac6d6cbbc1e59cc410736ec4f71f7b0d1a427e63
rsmulskii <transferave@gmail.com>
2024-07-13 10:58:45 +0300

    ++

--- bot/helpers.py
+++ bot/helpers.py
@@ -41,8 +41,10 @@ class Calculator:
 
     def get_cost_of_custom_house(self, yen_amount: int) -> float:
         # таможенный сбор
-        usd = yen_amount * self.currency.usd_per_yen
-        if usd > 190:
+        # usd = yen_amount * self.currency.usd_per_yen
+        # if usd > 190:
+
+        if self.over_limit(yen_amount, self.currency.euro_per_yen):
             self.cost_of_custom_house = yen_amount * custom_ratio
         return self.cost_of_custom_house
 
@@ -72,7 +74,6 @@ class Calculator:
 
     def over_limit(self, yen_amount, currency):
         limit = 200
-
         total = yen_amount * currency
         if total > limit:
             return True
